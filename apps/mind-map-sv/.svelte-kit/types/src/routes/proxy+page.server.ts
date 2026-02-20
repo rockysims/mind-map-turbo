c13ab2b60348b7c3b8bc64cfb1e7bce28899c6c1
rockysims <rocky.sims@gmail.com>
2026-02-19 20:42:10 -0800

    Some automatic changes in .svelte-kit and updated src/routes/page.svelte file very slightly.

--- apps/mind-map-sv/.svelte-kit/types/src/routes/proxy+page.server.ts
+++ apps/mind-map-sv/.svelte-kit/types/src/routes/proxy+page.server.ts
@@ -2,7 +2,8 @@
 import { env } from '$env/dynamic/private';
 import type { PageServerLoad } from './$types';	
 
-export const load = async ({ params }: Parameters<PageServerLoad>[0]) => {
+export const load = async () => {
 	const appName = env.APP_NAME || 'Fallback APP_NAME from server';
 	return {appName};
 };
+;null as any as PageServerLoad;
--- apps/mind-map-sv/src/routes/+page.svelte
+++ apps/mind-map-sv/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	const { data }: PageProps = $props();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit!</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 APP_NAME: {data.appName || 'Fallback APP_NAME from page'}<br/>
 <br/>
