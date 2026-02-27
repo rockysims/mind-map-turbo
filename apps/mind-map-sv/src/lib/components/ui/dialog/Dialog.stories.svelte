2b338425874fb81c6f6d0b48fc3ab5046b89a678
rockysims <rocky.sims@gmail.com>
2026-02-26 18:31:06 -0800

    Added dialog via shadcn (not used yet).

---
+++ apps/mind-map-sv/src/lib/components/ui/dialog/Dialog.stories.svelte
@@ -0,0 +1,18 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import DialogDemo from './DialogDemo.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/Dialog',
+		component: DialogDemo,
+		tags: []
+	});
+</script>
+
+<Story name="Closed">
+	<DialogDemo />
+</Story>
+
+<Story name="Open">
+	<DialogDemo initialOpen={true} />
+</Story>
---
+++ apps/mind-map-sv/src/lib/components/ui/dialog/DialogDemo.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import * as Dialog from './index.js';
+	let { initialOpen }: { initialOpen?: boolean } = $props();
+	let open = $state((() => initialOpen ?? false)());
+	// let open = true;
+	//TODO: figure out why initialOpen is not working
+</script>
+
+<Dialog.Root bind:open>
+	<Dialog.Trigger>Open dialog</Dialog.Trigger>
+	<Dialog.Content>
+		<Dialog.Header>
+			<Dialog.Title>Dialog title</Dialog.Title>
+			<Dialog.Description>
+				Placeholder description for the dialog in Storybook.
+			</Dialog.Description>
+		</Dialog.Header>
+	</Dialog.Content>
+</Dialog.Root>
